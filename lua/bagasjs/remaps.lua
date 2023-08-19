142229237123bb9a65351313e815b1763bbf6006
bagasjs <nakanoyoichi83@gmail.com>
2023-08-19 13:46:17 +0700

    Change some remaps and the file browser into telescope

--- lua/bagasjs/remaps.lua
+++ lua/bagasjs/remaps.lua
@@ -1,38 +1,42 @@
-vim.keymap.set({'i', 'v'}, '<C-c>', '<ESC><ESC>', { silent = true })
+local map = function (modes, key, command, config)
+    vim.keymap.set(modes, key, command, config)
+end
 
-vim.keymap.set({'n', 'i', 'v'}, '<C-s>', '<CMD>w<CR><ESC>', { silent = true, desc = "Save file" })
+map({'i', 'v'}, '<C-c>', '<ESC><ESC>', { silent = true })
+
+map({'n', 'i', 'v'}, '<C-s>', '<CMD>w<CR><ESC>', { silent = true, desc = "Save file" })
 
 -- Enter file explorer in normal mode
-vim.keymap.set('n', '<leader>f', ':Ex<CR>', { silent = true })
+map('n', '<leader>fe', ':Ex<CR>', { silent = true })
 
 -- better up/down
-vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Move Lines
-vim.keymap.set("n", "<M-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-vim.keymap.set("n", "<M-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-vim.keymap.set("i", "<M-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-vim.keymap.set("i", "<M-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-vim.keymap.set("v", "<M-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-vim.keymap.set("v", "<M-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+map("n", "<M-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<M-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<M-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<M-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<M-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<M-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- better indenting
-vim.keymap.set("v", ">", "<gv")
-vim.keymap.set("v", "<", ">gv")
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 -- windows
-vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Destoy window", remap = true })
-vim.keymap.set("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
-vim.keymap.set("n", "<leader>wh", "<C-W>s", { desc = "Split window horizontally", remap = true })
-vim.keymap.set("n", "<leader>wv", "<C-W>v", { desc = "Split window vertically", remap = true })
-vim.keymap.set("n", "<leader>w[", "<C-w>h", { desc = "Go to left window", remap = true })
-vim.keymap.set("n", "<leader>w]", "<C-w>l", { desc = "Go to right window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Destoy window", remap = true })
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
+map("n", "<leader>wh", "<C-W>s", { desc = "Split window horizontally", remap = true })
+map("n", "<leader>wv", "<C-W>v", { desc = "Split window vertically", remap = true })
+map("n", "<leader>w[", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<leader>w]", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- tabs
-vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-vim.keymap.set("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
-vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
